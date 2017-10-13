313752110c7c7d63be4e1e8d63c1a46f818af728
Luciano Strika <lucianostrika44@gmail.com>
2017-10-12 23:34:42 -0300

    article version

--- thready.py
+++ thready.py
@@ -1,42 +1,30 @@
 import multiprocessing
-from multiprocessing import Pool
-
+from PIL import Image, ImageDraw
 import time
-def randomFn(n):
-	for i in range(2000):
-		print 'so random! lol '+str(n)
-
-def expo(a):
-	return 2**a
-
-
-def runOnPool(n,f,l):
-	p = Pool(n)
-	b = time.time()
-	p.map(f,l)
-	e = time.time()
-	print(str(e-b))
+def slowFn(n):
+	sumTotal = sum([1 for _ in range(n)])
 
 
 if __name__ == '__main__':
     lista = []
-    '''for i in range(1000):
-    	lista.append(i)
-    for j in range(31):
-    	print(str(j))	
-    	runOnPool(j+1,expo,lista)'''
     res = []
     b = time.time()
-    for i in range(3000):
-        p = multiprocessing.Process(target=randomFn,args=(i,))
-        p.start()
+    m,n = 500,100000
+    for i in range(m):
+	p = multiprocessing.Process(target=slowFn,args=(n,))
+	p.start()
     e = time.time()
     res.append(str(e-b)) 
     b = time.time()
-    for i in range(3000):
-    	randomFn(i)
+    for i in range(m):
+    	slown(n)
     e = time.time()
-    res.append(str(e-b)) 	
+    res.append(str(e-b)) 
     print res
+
+
+'''
+	for article
     
-	
+
+'''
